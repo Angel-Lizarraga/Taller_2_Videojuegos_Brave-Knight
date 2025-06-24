fe745e8c89bf633a36a37a1824764cad5dd39614
VegaAxel318 <AxelEVega318@gmail.com>
2025-06-24 15:38:10 -0700

    Pitfalls
    uwu

--- scenes/Pitfall/pitfall.gd
+++ scenes/Pitfall/pitfall.gd
@@ -2,7 +2,9 @@ extends Node2D
 
 @onready var area2d : Area2D
 @onready var player_ref : Player
+@export var respawnPoint : Marker2D #Assigned in level
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	print("AAAAAAAAAAAAAAAAAAAAAAAAAAA")
+	area.get_parent().global_position = respawnPoint.global_position
 	
